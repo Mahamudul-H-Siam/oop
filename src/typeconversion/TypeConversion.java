1357d05af8cf322546177ab82af4cac01a39c9b3
unknown <ssiam8474@gmail.com>
2024-07-15 17:44:15 +0600

    Type conversion practice

---
+++ src/typeconversion/TypeConversion.java
@@ -0,0 +1,11 @@
+package typeconversion;
+
+public class TypeConversion {
+        void convert(double a, double b){
+            System.out.println("String to Integer conversion: " + (a+b));
+        }
+
+        void convert(float a, float b){
+            System.out.println("String to Integer conversion: " + (a+b));
+        }
+}
---
+++ src/typeconversion/TypeConversionTest.java
@@ -0,0 +1,9 @@
+package typeconversion;
+
+public class TypeConversionTest {
+    public static void main(String[] args) {
+        TypeConversion ob= new TypeConversion();
+        ob.convert(10, 15);
+        ob.convert(10, 30);
+    }
+}
